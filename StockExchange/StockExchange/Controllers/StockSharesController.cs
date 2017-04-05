50aab336b9c455806c6a3725494884ed39824dc1
AlexGF <63664@grundfos.com>
2017-04-05 16:25:06 +0200

    Initial commit of StockExchange

---
+++ StockExchange/StockExchange/Controllers/StockSharesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StockExchange.Models;
+using Microsoft.AspNet.Identity;
+
+namespace StockExchange.Controllers
+{
+    public class StockSharesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: StockShares
+        public async Task<ActionResult> Index()
+        {
+            string userid = User.Identity.GetUserId();
+            return View(await db.StockShares.Where(u => u.Users.Id == userid).ToListAsync());
+        }
+
+        // GET: StockShares/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockShares stockShares = await db.StockShares.FindAsync(id);
+            if (stockShares == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockShares);
+        }
+
+        // GET: StockShares/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StockShares/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Share")] StockShares stockShares)
+        {
+            if (ModelState.IsValid)
+            {
+                stockShares.Id = Guid.NewGuid();
+                db.StockShares.Add(stockShares);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(stockShares);
+        }
+
+        // GET: StockShares/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockShares stockShares = await db.StockShares.FindAsync(id);
+            if (stockShares == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockShares);
+        }
+
+        // POST: StockShares/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Share")] StockShares stockShares)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stockShares).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(stockShares);
+        }
+
+        // GET: StockShares/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockShares stockShares = await db.StockShares.FindAsync(id);
+            if (stockShares == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockShares);
+        }
+
+        // POST: StockShares/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            StockShares stockShares = await db.StockShares.FindAsync(id);
+            db.StockShares.Remove(stockShares);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StockExchange/StockExchange/Controllers/StocksController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StockExchange.Models;
+
+namespace StockExchange.Controllers
+{
+    public class StocksController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Stocks
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Stocks.ToListAsync());
+        }
+
+        // GET: Stocks/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = await db.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // GET: Stocks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stocks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,ShortName,Rate")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                stock.Id = Guid.NewGuid();
+                db.Stocks.Add(stock);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(stock);
+        }
+
+        // GET: Stocks/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = await db.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: Stocks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,ShortName,Rate")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stock).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(stock);
+        }
+
+        // GET: Stocks/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = await db.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Stock stock = await db.Stocks.FindAsync(id);
+            db.Stocks.Remove(stock);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
