e888ae21efaa015cdd8d811a2478540c41492a5f
Alex Karlsen <alexkarlsen7@gmail.com>
2017-04-18 16:36:44 +0200

    Initial stock exchange

---
+++ StockExchange/StockExchange/Models/Stock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace StockExchange.Models
+{
+	public class StockContext : DbContext
+	{
+		public StockContext(DbContextOptions<StockContext> options)
+			: base(options)
+		{ }
+
+		public DbSet<Stock> Stocks { get; set; }
+	}
+
+	public class Stock
+	{
+		public Stock()
+		{
+		}
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string ShortName { get; set; }
+		public double Rate { get; set; }
+	}
+}
